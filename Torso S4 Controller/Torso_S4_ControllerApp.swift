815c46ecb49a48cb7d3a273b8002aa30fac6e340
Bryan Abdul Collins <bryanabdul@gmail.com>
2026-03-13 09:28:35 -0400

    Initial Commit

---
+++ Torso S4 Controller/Torso_S4_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Torso_S4_ControllerApp.swift
+//  Torso S4 Controller
+//
+//  Created by Bryan Abdul Collins on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Torso_S4_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Torso S4 ControllerTests/Torso_S4_ControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Torso_S4_ControllerTests.swift
+//  Torso S4 ControllerTests
+//
+//  Created by Bryan Abdul Collins on 3/13/26.
+//
+
+import Testing
+@testable import Torso_S4_Controller
+
+struct Torso_S4_ControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
